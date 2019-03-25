42efdd91a7c209f6d2b684b38e5117ac6d424b3c
arush agarwal <agarwal.arush98@gmail.com>
2019-03-25 23:33:42 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app=express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = now+" "+req.method+" "+req.path;
+    fs.appendFile('server.log',log+'\n',err=>{
+        if(err) console.log(err);
+    })
+    next();
+})
+
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMessage:'Welcome to my Website',
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:"About page", 
+    });
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errormessage:"Unable to send message"
+    })
+})
+
+app.listen(3100,()=>{
+    console.log("Server started");
+});
